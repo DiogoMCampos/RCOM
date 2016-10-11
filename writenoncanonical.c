898a08cf39ba59532db2d266f4549d2695ae42a6
Diogo Campos <d.diogocampos@gmail.com>
2016-10-11 16:18:22 +0100

    Better indentation

--- writenoncanonical.c
+++ writenoncanonical.c
@@ -19,18 +19,18 @@
 
 volatile int STOP=FALSE;
 
-int main(int argc, char** argv)
+int main (int argc, char** argv)
 {
     int fd, c, res;
     struct termios oldtio, newtio;
     char buf[255];
     int i, sum = 0, speed = 0;
     
-    if ( (argc < 2) || 
+    if ((argc < 2) || 
   	     ((strcmp("/dev/ttyS0", argv[1])!=0) && 
-  	      (strcmp("/dev/ttyS1", argv[1])!=0) )) {
-      printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
-      exit(1);
+  	      (strcmp("/dev/ttyS1", argv[1])!=0))) {
+        printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
+        exit(1);
     }
 
 
@@ -40,12 +40,16 @@ int main(int argc, char** argv)
   */
 
 
-    fd = open(argv[1], O_RDWR | O_NOCTTY );
-    if (fd <0) {perror(argv[1]); exit(-1); }
+    fd = open(argv[1], O_RDWR | O_NOCTTY);
+   
+    if (fd < 0) {
+        perror(argv[1]);
+        exit(-1);
+    }
 
-    if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
-      perror("tcgetattr");
-      exit(-1);
+    if (tcgetattr(fd, &oldtio) == -1) { /* save current port settings */
+        perror("tcgetattr");
+        exit(-1);
     }
 
     bzero(&newtio, sizeof(newtio));
@@ -56,28 +60,22 @@ int main(int argc, char** argv)
     /* set input mode (non-canonical, no echo,...) */
     newtio.c_lflag = 0;
 
-    newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
-    newtio.c_cc[VMIN]     = 5;   /* blocking read until 5 chars received */
-
-
+    newtio.c_cc[VTIME] = 0;   /* inter-character timer unused */
+    newtio.c_cc[VMIN] = 5;   /* blocking read until 5 chars received */
 
   /* 
     VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a 
     leitura do(s) próximo(s) caracter(es)
   */
 
-
-
     tcflush(fd, TCIOFLUSH);
 
-    if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
+    if (tcsetattr(fd, TCSANOW, &newtio) == -1) {
       perror("tcsetattr");
       exit(-1);
     }
 
     printf("New termios structure set\n");
-
-
 
     gets(buf);
     
@@ -91,17 +89,11 @@ int main(int argc, char** argv)
     O ciclo FOR e as instruções seguintes devem ser alterados de modo a respeitar 
     o indicado no guião 
   */
-
-
-
    
-    if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
-      perror("tcsetattr");
-      exit(-1);
+    if (tcsetattr(fd, TCSANOW, &oldtio) == -1) {
+        perror("tcsetattr");
+        exit(-1);
     }
-
-
-
 
     close(fd);
     return 0;
