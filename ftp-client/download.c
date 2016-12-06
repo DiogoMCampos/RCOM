c449f3571c80070f782969f538eb38a0166dea91
Diogo Campos <d.diogocampos@gmail.com>
2016-12-06 17:15:41 +0000

    Parsing URL correctly

--- ftp-client/download.c
+++ ftp-client/download.c
@@ -14,9 +14,48 @@ int main(int argc, char **argv) {
     char* host = malloc(HOST_MAX * sizeof(char));
     char* path = malloc(PATH_MAX * sizeof(char));
 
-    parseUrl(url, host, user, password, path);
+    parseUrl(url, user, password, host, path);
+
+    return 0;
 }
 
-void parseUrl(char* url, char* host, char* user, char* password, char* path) {
+void parseUrl(char* url, char* user, char* password, char* host, char* path) {
+    int state = USER_STATE;
+    int i = 0;
 
+    while (*url != '\0') {
+        switch (state) {
+            case USER_STATE:
+                if (*url == ':') {
+                    i = 0;
+                    state = PASS_STATE;
+                } else {
+                    user[i++] = *url;
+                }
+                ++url;
+                break;
+            case PASS_STATE:
+                if (*url == '@') {
+                    i = 0;
+                    state = HOST_STATE;
+                } else {
+                    password[i++] = *url;
+                }
+                ++url;
+                break;
+            case HOST_STATE:
+                if (*url == '/') {
+                    i = 0;
+                    state = PATH_STATE;
+                } else {
+                    host[i++] = *url;
+                }
+                ++url;
+                break;
+            case PATH_STATE:
+                path[i++] = *url;
+                ++url;
+                break;
+        }
+    }
 }
--- ftp-client/download.h
+++ ftp-client/download.h
@@ -2,10 +2,17 @@
 #include <stdio.h>
 #include <string.h>
 
+// Max Length for URL parts
 #define URL_MAX 256
 #define USER_MAX 32
 #define PASS_MAX 32
 #define HOST_MAX 64
 #define PATH_MAX 128
 
-void parseUrl(char* url, char* host, char* user, char* password, char* path);
+// URL State Machine
+#define USER_STATE 0
+#define PASS_STATE 1
+#define HOST_STATE 2
+#define PATH_STATE 3
+
+void parseUrl(char* url, char* user, char* password, char* host, char* path);
